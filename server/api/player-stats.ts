bee64b385864ab2652e26eb0e87a4a8147430bff
Manuel <mannuehkipkirui@gmail.com>
2025-01-23 23:56:16 +0300

    Adding player stats

---
+++ server/api/player-stats.ts
@@ -0,0 +1,34 @@
+export default defineEventHandler(async (event) => {
+    try {
+      const query = getQuery(event);
+      const playerId = query.playerId;
+  
+      if (!playerId) {
+        return createError({
+          statusCode: 400,
+          statusMessage: "player id is required",
+        });
+      }
+  
+      const apiUrl = 'https://api.football-data.org/v4';
+      const headers = {
+        "X-Auth-Token": process.env.API_KEY || '',
+      };
+  
+      if (!process.env.API_KEY) {
+        return createError({
+          statusCode: 500,
+          statusMessage: "API_KEY is not defined",
+        });
+      }
+      const response = await $fetch(`${apiUrl}/persons/${playerId}`, { headers });
+  
+      return response;
+    } catch (error: any) {
+      return createError({
+        statusCode: error.response?.status || 500,
+        statusMessage: "Failed to fetch player data",
+      });
+    }
+  });
+  
